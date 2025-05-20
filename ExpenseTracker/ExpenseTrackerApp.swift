6f6ebf4b65dd54c8c9d5f2b49f7269b4c6771a6e
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-05-20 07:49:27 +0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Gurur on 20.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
